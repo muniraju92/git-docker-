0943e2016e0dfbf8c9219cc17ab65d9656a95e81
muniraju <muniraju.araveti92@gmail.com>
2020-01-13 11:50:26 +0000

    1st

---
+++ src/main/java/mvnprojectdocker20/App.java
@@ -0,0 +1,13 @@
+package mvnprojectdocker20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
